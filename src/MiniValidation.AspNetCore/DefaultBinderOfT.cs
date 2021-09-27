00be2f9e070e6adb3318b9cf140097543d0addab
Damian Edwards <damian@damianedwards.com>
2021-09-26 22:33:12 -0700

    Just use HttpContext.Items for storing result of default binding

--- src/MiniValidation.AspNetCore/DefaultBinderOfT.cs
+++ src/MiniValidation.AspNetCore/DefaultBinderOfT.cs
@@ -4,23 +4,19 @@ namespace MiniValidationLib.AspNetCore;
 
 internal static class DefaultBinder<TValue>
 {
+    private static object _itemsKey = new();
     private static readonly RequestDelegate _defaultRequestDelegate = RequestDelegateFactory.Create(DefaultValueDelegate).RequestDelegate;
 
     public static async Task<TValue?> GetValueAsync(HttpContext httpContext)
     {
-        var boundValueHttpContext = new BoundValueHttpContext<TValue>(httpContext);
+        await _defaultRequestDelegate(httpContext);
 
-        await _defaultRequestDelegate(boundValueHttpContext);
-
-        return boundValueHttpContext.Value;
+        return (TValue?)httpContext.Items[_itemsKey];
     }
 
     private static IResult DefaultValueDelegate(TValue value, HttpContext httpContext)
     {
-        if (httpContext is BoundValueHttpContext<TValue> boundValueHttpContext)
-        {
-            boundValueHttpContext.Value = value;
-        }
+        httpContext.Items[_itemsKey] = value;
         
         return FakeResult.Instance;
     }
